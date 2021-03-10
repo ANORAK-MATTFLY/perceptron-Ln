62fa6f4df46135b1890f06c514590a5551acfdd1
ANORAK-MATTFLY <benmatanda354@gmail.com>
2021-03-10 17:55:24 +0100

    Adding a navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,40 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import style from '../styles/index.module.scss';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAt, faCoffee, faHome, faTags } from '@fortawesome/free-solid-svg-icons'
+
+
 
 export default function Home() {
   return (
     <div id={style.container}>
       <nav>
         {/* Main title */}
-        <h1><a href="#">Perceptron@Ln</a></h1>
+        <Link href="#">
+        <a>Perceptron@Ln</a>
+        </Link>
         {/* navbar links */}
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Tags</a></li>
-          <li><a href="#">About_me</a></li>
+          <li>
+            <FontAwesomeIcon icon={faHome} className="LinkIcon" />
+            <Link href="#">
+              <a href="#">Home</a>
+            </Link>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faTags} className="LinkIcon" />
+            <Link href="#">
+              <a href="#">Tags</a>
+            </Link>
+          </li>
+          <Link href="#">
+          <li>
+            <FontAwesomeIcon icon={faAt} className="LinkIcon" />
+              <a href="#">About_me</a>
+          </li>
+          </Link>
         </ul>
       </nav>
     </div>
