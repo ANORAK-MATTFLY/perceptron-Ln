990fd49451e526767f2d12e6e97b03f736ee70e8
ANORAK-MATTFLY <benmatanda354@gmail.com>
2021-03-10 18:41:43 +0100

    Sass code refactore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,64 @@
-import Head from 'next/head'
-import style from '../styles/index.module.scss';
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAt, faCoffee, faHome, faTags } from '@fortawesome/free-solid-svg-icons'
-
-
+import Head from "next/head";
+import style from "../styles/index.module.scss";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAt,
+  faCoffee,
+  faHome,
+  faSearch,
+  faTags,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <div id={style.container}>
       <head>
-<title></title>
-<meta name="description" content="" />
-<meta property="og:type" content="website" />
-<meta name="og:title" property="og:title" content="" />
-<meta name="og:description" property="og:description" content="" />
-<meta property="og:site_name" content="" />
-<meta property="og:url" content="" />  
-<meta name="twitter:card" content="summary" /> 
-<meta name="twitter:title" content="" />
-<meta name="twitter:description" content={props.desc} />
-<meta name="twitter:site" content="" />
-<meta name="twitter:creator" content="" />
-<link rel="icon" type="image/png" href="/static/images/favicon.ico" />
-<link rel="apple-touch-icon" href="/static/images/favicon.ico" />
-<link rel="stylesheet" href="" />
-<meta property="og:image" content="" />  
-<meta name="twitter:image" content="" />   
-<link rel="canonical" href="" />
-<script type="text/javascript" src="" ></script>
-</head>
+        <title>Perceptron@Ln</title>
+        <meta
+          name="description"
+          content="A blog to help people interested in computer science"
+        />
+      </head>
       <nav>
-        {/* Main title */}
+        {/* Logo side */}
         <Link href="#">
-        <a>Perceptron@Ln</a>
+          <a>Perceptron@Ln</a>
         </Link>
+
+        {/* search bar */}
+        <div id="search-bar-container">
+          <input type="text" placeholder="Search" />
+          <FontAwesomeIcon icon={faSearch} className="LinkIcon" />
+        </div>
         {/* navbar links */}
         <ul>
           <li>
             <Link href="#">
               <a href="#">
-                  <FontAwesomeIcon icon={faHome} className="LinkIcon" />
-                Home</a>
+                <FontAwesomeIcon icon={faHome} className="LinkIcon" />
+                Home
+              </a>
             </Link>
           </li>
           <li>
             <Link href="#">
               <a href="#">
                 <FontAwesomeIcon icon={faTags} className="LinkIcon" />
-                Tags</a>
+                Tags
+              </a>
             </Link>
           </li>
           <Link href="#">
-          <li>
+            <li>
               <a href="#">
                 <FontAwesomeIcon icon={faAt} className="LinkIcon" />
-                About_me</a>
-          </li>
+                About_me
+              </a>
+            </li>
           </Link>
         </ul>
       </nav>
     </div>
-  )
+  );
 }
