eb397b1ff6ed2141520e7b741961584220b9ebc4
ANORAK-MATTFLY <benmatanda354@gmail.com>
2021-03-10 18:08:22 +0100

    Aligned navbar links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import style from '../styles/index.module.scss';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,6 +9,27 @@ import { faAt, faCoffee, faHome, faTags } from '@fortawesome/free-solid-svg-icon
 export default function Home() {
   return (
     <div id={style.container}>
+      <head>
+<title></title>
+<meta name="description" content="" />
+<meta property="og:type" content="website" />
+<meta name="og:title" property="og:title" content="" />
+<meta name="og:description" property="og:description" content="" />
+<meta property="og:site_name" content="" />
+<meta property="og:url" content="" />  
+<meta name="twitter:card" content="summary" /> 
+<meta name="twitter:title" content="" />
+<meta name="twitter:description" content={props.desc} />
+<meta name="twitter:site" content="" />
+<meta name="twitter:creator" content="" />
+<link rel="icon" type="image/png" href="/static/images/favicon.ico" />
+<link rel="apple-touch-icon" href="/static/images/favicon.ico" />
+<link rel="stylesheet" href="" />
+<meta property="og:image" content="" />  
+<meta name="twitter:image" content="" />   
+<link rel="canonical" href="" />
+<script type="text/javascript" src="" ></script>
+</head>
       <nav>
         {/* Main title */}
         <Link href="#">
@@ -18,21 +38,24 @@ export default function Home() {
         {/* navbar links */}
         <ul>
           <li>
-            <FontAwesomeIcon icon={faHome} className="LinkIcon" />
             <Link href="#">
-              <a href="#">Home</a>
+              <a href="#">
+                  <FontAwesomeIcon icon={faHome} className="LinkIcon" />
+                Home</a>
             </Link>
           </li>
           <li>
-            <FontAwesomeIcon icon={faTags} className="LinkIcon" />
             <Link href="#">
-              <a href="#">Tags</a>
+              <a href="#">
+                <FontAwesomeIcon icon={faTags} className="LinkIcon" />
+                Tags</a>
             </Link>
           </li>
           <Link href="#">
           <li>
-            <FontAwesomeIcon icon={faAt} className="LinkIcon" />
-              <a href="#">About_me</a>
+              <a href="#">
+                <FontAwesomeIcon icon={faAt} className="LinkIcon" />
+                About_me</a>
           </li>
           </Link>
         </ul>
