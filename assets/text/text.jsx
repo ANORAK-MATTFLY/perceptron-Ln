74647d8ca19edc447409a717d3a328b06b7f43d7
Ben Matanda <benmatanda354@gmail.com>
2021-08-26 00:48:33 +0100

    style: about section

---
+++ assets/text/text.jsx
@@ -0,0 +1,6 @@
+const AboutTexts = {
+    greetings: "Hello 👋, I'm Ben. Nice to meet you!",
+    about_me: "Since beginning my journey as a software Engineer nearly 3 years ago, I've worked remotely, worked as a freelancer, worked in a startup, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my skills to give better results in my career.",
+}
+
+export default AboutTexts;
--- components/about_page/about_body.jsx
+++ components/about_page/about_body.jsx
@@ -1,5 +1,16 @@
 import style from '../../styles/about.module.scss';
-
+import AboutTexts from '../../assets/text/text';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faAndroid,
+    faBlogger,
+    faChrome,
+    faDev,
+    faGithub,
+    faTwitter,
+    faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { faBlog, faMobile, faPhone, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
 
 const AboutBody = () => {
     return (<div id={style.container}>
@@ -19,6 +30,48 @@ const AboutBody = () => {
                 }}
             ></div>
         </article>
+        <div className={style.about_me}>
+            <h1>{AboutTexts.greetings}</h1>
+            <p>{AboutTexts.about_me}</p>
+            <div id={style.skills_and_strengths}>
+                <div id={style.main_rows}>
+                    <div id={style.row_head}><FontAwesomeIcon icon={faMobile} id={style.icon} />
+                        <h2>Mobile</h2>
+                    </div>
+                    <div id={style.description}>
+                        <p>
+                            I value simple content structure, clean design patterns, and thoughtful interactions.
+                        </p>
+                    </div>
+                </div>
+                <div id={style.main_rows}>
+                    <div id={style.row_head}>
+                        <FontAwesomeIcon icon={faChrome} id={style.icon} />
+                        <h2>Web</h2>
+                    </div>
+                    <div id={style.description}>
+                        <p>
+                            I like to code things from scratch, and enjoy bringing ideas to life in the browser.
+                        </p>
+                    </div>
+                </div>
+                <div id={style.main_rows}>
+                    <div id={style.row_head}>
+                        <FontAwesomeIcon icon={faYoutube} id={style.icon} />
+                        <h2>Mentor</h2>
+                    </div>
+                    <div id={style.description}>
+                        <p>
+                            I genuinely care about people, and love helping fellow designers work on their craft through my
+                        </p>
+                        <a href="https://www.youtube.com/channel/UCaZslnwpO3OvakjFYWV2uJA" target="_blank">
+                            Youtube channel
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>);
 };
 
