b1886748f26696a645e1c5b6cfc95137b0d846d1
Ben Matanda <benmatanda354@gmail.com>
2021-09-04 20:53:55 +0100

    style: responsive

--- common/md-to-html/md-processor.js
+++ common/md-to-html/md-processor.js
@@ -3,9 +3,10 @@ import remark from 'remark';
 import html from 'remark-html';
 import remarkImages from 'remark-images';
 
-export default async function markdownToHtml(markdown, filePath) {
+
+export default async function markdownToHtml(markdownContent, filePath) {
     const file = readSync(filePath);
     await remark().use(remarkImages).process(file);
-    const result = await remark().use(html).process(markdown)
+    const result = await remark().use(html).process(markdownContent)
     return result.toString()
 }
